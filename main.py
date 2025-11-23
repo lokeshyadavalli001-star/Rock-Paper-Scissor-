0609c6960db8983a056c8dcfceac427660120e2e
lokeshyadavalli001-star <lokeshyadavalli001@gmail.com>
2025-11-23 16:01:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 import random
 import time
-play_again=True
+
+player_score = 0
+computer_score = 0
+tie_score = 0
+
+play_again = True
+
 while play_again:
     print("Welcome to Rock Paper Scissors! Game")
     time.sleep(1)
+    
     while True:
         user = input("Choose anything from these (r/p/s): ").lower()
         if user in ['r', 'p', 's']:
             break
         else:
             print("Invalid choice! Please choose from (r/p/s)")
+    
     user_choice = user
+    
     if user == 'r':
         user += '🪨'
     elif user == 'p':
         user += '📃'
     elif user == 's':
         user += '✂️'
+    
     options = ['r🪨', 'p📃', 's✂️']
+    
     print("Computer is choosing...")
     time.sleep(1)
     computer = random.choice(options)
@@ -25,28 +36,42 @@ while play_again:
     time.sleep(1)
     print("Please wait...")
     time.sleep(1)
+    
     if user == computer:
-        print("You won!")
+        print("It's a tie!")
+        tie_score += 1
         print("You chosen " + user)
         print("Computer chosen " + computer)
     else:
         if user_choice == 'r':
-            if computer == 'r🪨':
-                print("You won!")
-            else:
-                print("You lost")
-        elif user_choice == 'p':
-            if computer == 'p📃':
-                print("You won!")
-            else:
-                print("You lost")
-        elif user_choice == 's':
             if computer == 's✂️':
                 print("You won!")
+                player_score += 1
             else:
                 print("You lost")
+                computer_score += 1
+        elif user_choice == 'p':
+            if computer == 'r🪨':
+                print("You won!")
+                player_score += 1
+            else:
+                print("You lost")
+                computer_score += 1
+        elif user_choice == 's':
+            if computer == 'p📃':
+                print("You won!")
+                player_score += 1
+            else:
+                print("You lost")
+                computer_score += 1
+        
         print("You chosen " + user)
         print("Computer chosen " + computer)
+    
+    print(f"\n--- Current Scores ---")
+    print(f"Player: {player_score} | Computer: {computer_score} | Ties: {tie_score}")
+    print("----------------------\n")
+    
     while True:
         play_again_input = input("Do you want to play again? (y/n): ").lower()
         if play_again_input == 'y':
@@ -54,6 +79,9 @@ while play_again:
             break
         elif play_again_input == 'n':
             print("Bye! Have a nice day!")
+            print(f"\n=== Final Scores ===")
+            print(f"Player: {player_score} | Computer: {computer_score} | Ties: {tie_score}")
+            print("Thanks for playing! 🎮")
             play_again = False
             break
         else:
